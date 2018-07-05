ba905a674eeb29c659323c4b66fecef0a12670d5
Dilip Kola <dilip@tensult.com>
2018-07-05 12:53:32 +0530

    Add async failure component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 module.exports['asynccomp'] = require('./asynccomp/index.marko');
+module.exports['asyncfailure'] = require('./asyncfailure/index.marko');
 module.exports['foo'] = require('./foo/index.marko');
 module.exports['bar'] = require('./bar/index.marko');
