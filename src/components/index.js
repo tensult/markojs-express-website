a6b593459e7156cc9ef752fc1f9eccaa6270d8c1
Dilip Kola <dilip@tensult.com>
2018-07-05 09:59:14 +0530

    Add async component example

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
+module.exports['asynccomp'] = require('./asynccomp/index.marko');
 module.exports['foo'] = require('./foo/index.marko');
 module.exports['bar'] = require('./bar/index.marko');
--- src/components/load-component/index.js
+++ src/components/load-component/index.js
@@ -1,6 +1,6 @@
 const components = require('../index');
 
-module.exports = function (input, out) {
+module.exports = async function(input, out) {
     const componentId = input.componentId;
     if (!components[componentId]) {
         console.error(componentId, "component doesn't exist");
@@ -8,9 +8,9 @@ module.exports = function (input, out) {
     } else {
         out.write(`<div class='${componentId}'>`);
         try {
-            components[componentId].render(input.data, out);
+            await components[componentId].render(input.data, out)
         } catch (err) {
-            console.error(`Error occurred while loading component=${componentId} with data=${data}`, err);
+            console.error(`Error occurred while loading component=${componentId} with data=${input}`, err);
         }
         out.write('</div>');
     }
