b21c9f49b1125ede5ef7eb799a8de74a27ca38a6
Dilip Kola <dilip@tensult.com>
2018-07-11 13:52:28 +0530

    Add JS component example

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 module.exports['asynccomp'] = require('./asynccomp/index.marko');
 module.exports['asyncfailure'] = require('./asyncfailure/index.marko');
-module.exports['foo'] = require('./foo/index.marko');
 module.exports['bar'] = require('./bar/index.marko');
+module.exports['foo'] = require('./foo/index.marko');
+module.exports['jscomp'] = require('./jscomp/index.marko');
