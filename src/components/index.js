932d2ced04ab01a95a797f0cbda886866fe27a80
Dilip Kola <dilip@tensult.com>
2018-07-16 12:42:51 +0530

    Add example for custom html tag

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 module.exports['asynccomp'] = require('./asynccomp/index.marko');
 module.exports['asyncfailure'] = require('./asyncfailure/index.marko');
 module.exports['bar'] = require('./bar/index.marko');
+module.exports['custom-html-tag'] = require('./custom-html-tag/index.marko');
 module.exports['foo'] = require('./foo/index.marko');
 module.exports['jscomp'] = require('./jscomp/index.marko');
+
